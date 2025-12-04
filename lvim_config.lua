01c57aee240052b37680241d1ac4f04d48098911
solractys <carloseduardosilvasouzadelima@gmail.com>
2025-12-04 15:58:31 -0300

    done

--- lvim_config.lua
+++ lvim_config.lua
@@ -2,3 +2,40 @@ lvim.transparent_window = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
+lvim.plugins = {
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({})
+        end,
+    },
+    
+    {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+            require("copilot_cmp").setup({
+                suggestion = { enabled = false },
+                panel = { enabled = false }
+            })
+        end
+    }
+}
+
+-- A configuração abaixo é necessária para evitar que o copilot substitua o Tab com uma sugestão
+-- quando você só está tentando indentar!
+local has_words_before = function()
+    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
+local on_tab = vim.schedule_wrap(function(fallback)
+    local cmp = require("cmp")
+    if cmp.visible() and has_words_before() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+    else
+        fallback()
+    end
+end)
+lvim.builtin.cmp.mapping["<Tab>"] = on_tab
